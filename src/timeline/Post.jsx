2aede3905ff6ca07cfda60a3cbd072e68d9f6453
brycemooore <brycemooore@gmail.com>
2021-01-04 11:19:08 -0500

    new styles

--- src/timeline/Post.jsx
+++ src/timeline/Post.jsx
@@ -1,55 +1,86 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+import cx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteBorderRounded from "@material-ui/icons/FavoriteBorderRounded";
+import Share from "@material-ui/icons/Share";
+import { useSoftRiseShadowStyles } from "@mui-treasury/styles/shadow/softRise";
+import { useSlopeCardMediaStyles } from "@mui-treasury/styles/cardMedia/slope";
+import { useN01TextInfoContentStyles } from "@mui-treasury/styles/textInfoContent/n01";
+import TextInfoContent from "@mui-treasury/components/content/textInfo";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(() => ({
   root: {
-    maxWidth: 400,
-    padding: "1px",
+    maxWidth: 304,
+    margin: "auto",
   },
-  media: {
-    height: 140,
+  content: {
+    padding: 24,
   },
-});
-
+  avatar: {
+    width: 50,
+    height: 50,
+    border: "2px solid #fff",
+    margin: "-48px 32px 0 auto",
+    "& > img": {
+      margin: 0,
+    },
+  },
+}));
 
 export default function Post() {
-    const classes = useStyles();
+  const cardStyles = useStyles();
+  const mediaStyles = useSlopeCardMediaStyles();
+  const shadowStyles = useSoftRiseShadowStyles();
+  const textCardContentStyles = useN01TextInfoContentStyles();
 
-    return (
-        <div style={{padding: "10px"}}>
-        <Card className={classes.root}>
-        <CardActionArea>
+  return (
+    <TimelineItem>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <Card className={cx(cardStyles.root, shadowStyles.root)}>
           <CardMedia
-            className={classes.media}
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Contemplative Reptile"
+            classes={mediaStyles}
+            image={
+              "https://images.unsplash.com/photo-1517147177326-b37599372b73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2229&q=80"
+            }
           />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-              across all continents except Antarctica
-            </Typography>
+          <Avatar
+            className={cardStyles.avatar}
+            src={"https://i.pravatar.cc/300"}
+          />
+          <CardContent className={cardStyles.content}>
+            <TextInfoContent
+              classes={textCardContentStyles}
+              heading={"First Snow Storm"}
+              body={
+                "Snow storm coming in Sommaroy island, Arctic Norway. This is something that you definitely wanna see in your life."
+              }
+            />
           </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
-      </div>
-    );
+          <Box px={2} pb={2} mt={-1}>
+            <IconButton>
+              <Share />
+            </IconButton>
+            <IconButton>
+              <FavoriteBorderRounded />
+            </IconButton>
+          </Box>
+        </Card>
+      </TimelineContent>
+    </TimelineItem>
+  );
 }
--- src/timeline/Section.jsx
+++ src/timeline/Section.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./output.css";
 import Post from './Post'
 
+
 export default function Section(props) {
   return (
       <section className={`section section${props.id}`} id={`section${props.id}`}>
