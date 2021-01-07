0251d711e652576eb52b5ed3c048966dca6e80a2
raaynaldo <raaynaldo@hotmail.com>
2021-01-07 11:28:06 -0500

    fix the year scroll

--- src/timeline/SectionLine.jsx
+++ src/timeline/SectionLine.jsx
@@ -3,9 +3,17 @@ import Section from "./Section";
 
 export default function SectionLine(props) {
   function generateSections() {
-    return Object.entries(props.sections).reverse().map(([key, value], index) => (
-      <Section key={key} year={key} index={index} timeline={value} user={props.user}/>
-    ));
+    return Object.entries(props.sections)
+      .reverse()
+      .map(([key, value], index) => (
+        <Section
+          key={key}
+          year={key}
+          index={index}
+          timeline={value}
+          user={props.user}
+        />
+      ));
   }
 
   return generateSections();
